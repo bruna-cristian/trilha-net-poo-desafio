748643ef2fa5b13e8d7cb7ccd0341f67fe22233a
bruna-cristian <156549317+bruna-cristian@users.noreply.github.com>
2024-01-22 22:43:12 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,8 @@
+﻿using DesafioPOO.Models;
+
+Nokia tijolao = new Nokia("34984389012", "Nokia 3310", "123456789", 4); 
+Iphone one = new Iphone("34992405060", "Iphone", "987654321", 8);
+
+tijolao.InstalarAplicativo("Jogo da cobrinha");
+
+one.InstalarAplicativo("Super Mario World");
